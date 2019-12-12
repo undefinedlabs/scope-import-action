2aeacf9772f34888dd7751557f3d6984305a619f
drodriguezhdez <daniel.rguez.hdez.github@gmail.com>
2019-12-12 15:56:42 +0100

    Add 'path' input

--- src/scope-for-junit.ts
+++ src/scope-for-junit.ts
@@ -5,6 +5,9 @@ async function run() {
         let dsn = core.getInput("dsn", {required: true})
         core.exportVariable("SCOPE_DSN", dsn);
 
+        let path = core.getInput("path", {required: true})
+
+
     } catch (error) {
         core.setFailed(error.message)
     }
