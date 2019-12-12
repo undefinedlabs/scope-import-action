54a1f5b0aaab6f981e892d2b8017f99a94737f80
drodriguezhdez <daniel.rguez.hdez.github@gmail.com>
2019-12-12 16:02:57 +0100

    using go install

--- src/scope-for-junit.ts
+++ src/scope-for-junit.ts
@@ -1,15 +1,20 @@
-import * as core from '@actions/core'
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
 
 async function run() {
     try {
-        let dsn = core.getInput("dsn", {required: true})
+
+        let dsn = core.getInput("dsn", {required: true});
         core.exportVariable("SCOPE_DSN", dsn);
 
-        let path = core.getInput("path", {required: true})
+        let path = core.getInput("path", {required: true});
 
+        await exec.exec("go get github.com/go-training/helloworld");
+        await exec.exec("go install github.com/go-training/helloworld");
+        await exec.exec("./helloworld");
 
     } catch (error) {
-        core.setFailed(error.message)
+        core.setFailed(error.message);
     }
 }
 
