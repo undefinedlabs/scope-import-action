c8a6d89295840ff159cc7e2f39a9ee5b488c9946
Daniel Rodriguez Hernandez <daniel.rguez.hdez.github@gmail.com>
2020-01-15 12:16:57 +0100

    Avoid tool compilation (#2)
    * Use downloadTool to execute scope-importer
    
    * Rename to scope-import-action
    
    * Fix action
    
    * Added execution mode
    
    * Apply formatter

---
+++ src/scope-import.ts
@@ -0,0 +1,36 @@
+import * as path from "path";
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as tc from "@actions/tool-cache";
+import * as io from "@actions/io";
+
+const IS_WINDOWS = process.platform === "win32";
+const IS_MACOS = process.platform === "darwin";
+
+async function run() {
+  try {
+    let dsn = core.getInput("dsn", { required: true });
+    await core.exportVariable("SCOPE_DSN", dsn);
+
+    let pathVar = core.getInput("path", { required: true });
+    const platform = IS_WINDOWS ? "windows" : IS_MACOS ? "darwin" : "linux";
+
+    let scopeImportToolPath;
+    scopeImportToolPath = await tc.downloadTool(
+      "https://home.undefinedlabs.com/download/scope-import/" +
+        platform +
+        "/x86_64"
+    );
+    if (IS_WINDOWS && !scopeImportToolPath.endsWith(".exe")) {
+      await io.mv(scopeImportToolPath, scopeImportToolPath + ".exe");
+      scopeImportToolPath = scopeImportToolPath + ".exe";
+    } else {
+      await exec.exec("chmod +x " + scopeImportToolPath);
+    }
+    await exec.exec(scopeImportToolPath + " --path " + pathVar);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
